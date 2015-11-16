b9081d6a6f3a6bcc4e2b6146d256ed3e2cbf3bb5
James Zhang <jchunzh@gmail.com>
2015-11-16 00:31:57 -0500

    Update folder structure and html to use django

--- views.py
+++ views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def create(request):
+	return render(request, 'PollApp/createpoll.html')
+
+	# Create your views here.
+def vote(request):
+	return render(request, 'PollApp/votepoll.html')
